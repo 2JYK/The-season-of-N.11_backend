4fe247a914e96e87d6155ba6f2b271338f571584
GyeongSuKim <rla04722@naver.com>
2022-06-29 23:05:03 +0900

    Feat : url, views.py 제어
    Article, Comment, Bookmark, Like / CRUD 제어, url 적용

--- article/urls.py
+++ article/urls.py
@@ -5,4 +5,10 @@ from article import views
 
 urlpatterns = [
     path('', views.ArticleView.as_view()),
+    path('comment/', views.CommentView.as_view()),
+    path('bookmark/', views.BookMarkView.as_view()),
+    path('like/', views.LikeView.as_view()),
+    path('comment/<comment_id>/', views.CommentView.as_view()),
+    path('bookmark/<bookmark_id>/', views.BookMarkView.as_view()),
+    path('like/<like_id>/', views.LikeView.as_view()),
 ]
