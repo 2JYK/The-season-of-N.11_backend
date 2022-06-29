1dcf36cc0e62db24104fe300584a5b936b0bfdee
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-29 11:45:32 +0900

    Feat : custom creator 생성

--- user/serializers.py
+++ user/serializers.py
@@ -9,3 +9,13 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True},
         }
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', '')
+        
+        user = UserModel(**validated_data)
+        user.set_password(password)
+        user.save()
+        
+        return user
+    
