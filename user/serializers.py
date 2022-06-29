1365e6faffcca56d1c5ddf07d1dd55efe9c02687
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-29 10:31:54 +0900

    Feat : UserSerializer 생성

---
+++ user/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import User as UserModel
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ["username", "password", "email", "fullname", "join_date"]
