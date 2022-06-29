f51826815f6e2b6df9ea2851c0cebebe01aa2afc
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-29 19:25:41 +0900

    Feat : jwt 관련 url 설정

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from user import views
+from user.views import SeasonTokenObtainPairView
+
 
 urlpatterns = [
     path('', views.UserView.as_view()),
-    path('login/', views.UserAPIView.as_view()),
+    # path('login/', views.UserAPIView.as_view()),
     path('logout/', views.UserAPIView.as_view()),
+    # simplejwt 에서 제공하는 기본 JWT 인증 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/season/token/', SeasonTokenObtainPairView.as_view(), name='season_token'),
+    path('api/authonly/', views.OnlyAuthenticatedUserView.as_view()),
 ]
 
