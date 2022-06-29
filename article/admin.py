852b9f26b0adfcc4f015b681c3cf268f752410ba
GyeongSuKim <rla04722@naver.com>
2022-06-29 23:05:45 +0900

    Feat : Like, BookMark Serializer

--- article/admin.py
+++ article/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from article.models import Article as ArticleModel
 from article.models import Comment as CommentModel
+from article.models import Like as LikeModel
+from article.models import BookMark as BookMarkModel 
+from django.contrib import admin
 
 
+# class BookmarkAdmin(admin.ModelAdmin):
+#     list_display= ("id","article", "user", )
+
+# class LikeAdmin(admin.ModelAdmin):
+#     list_display= ("id","article", "user")
+
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display= ("id", "article", "user")
+    
+# Bookmark클래스와 BookmarkAdmin클래스를 등록
+admin.site.register(BookMarkModel)
 admin.site.register(ArticleModel)
 admin.site.register(CommentModel)
+admin.site.register(LikeModel)
--- article/serializers.py
+++ article/serializers.py
@@ -1,19 +1,34 @@
 from rest_framework import serializers
 from article.models import Article as ArticleModel
 from article.models import Comment as CommentModel
+from article.models import Like as LikeModel
+from article.models import BookMark as BookMarkModel
 from user.serializers import UserSerializer
 
+
+class BookMarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookMarkModel
+        fields = "__all__"
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LikeModel
+        fields = "__all__"
 
 
 class CommentSerializer(serializers.ModelSerializer):
 
+    # user = serializers.SlugRelatedField(read_only=True, slug_field='fullname')  # id 값 안나올 시 삭제 ! 
     class Meta:
         model = CommentModel
         fields = "__all__"
 
+
 class ArticleSerializer(serializers.ModelSerializer):
     comment_set = CommentSerializer(many=True, read_only=True)
-    user = UserSerializer()
+    # user = UserSerializer(many=True)
     # image =
     class Meta:
         model = ArticleModel
