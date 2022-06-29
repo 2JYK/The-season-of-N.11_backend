8cbb2f365347100df1712eeec25c91fb776857a2
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-29 22:57:48 +0900

    Feat : django session 기반 로그인 및 로그아웃 삭제
    추가적으로 jwt 기반 view를 생성함

--- user/views.py
+++ user/views.py
@@ -52,28 +52,7 @@ class UserView(APIView):
             user.delete()
             return Response({"message": "회원탈퇴 성공"}, status=status.HTTP_200_OK)
         return Response({"message": "회원탈퇴 실패"}, status=status.HTTP_400_BAD_REQUEST)
-   
-class UserAPIView(APIView):
-    
-    # DONE 로그인
-    # def post(self, request):
-    #     username = request.data.get('username', '')
-    #     password = request.data.get('password', '')
-        
-    #     user = authenticate(request, username=username, password=password)
-        
-    #     if not user:
-    #         return Response({"msg": "존재하지 않는 계정이거나 비밀번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
-        
-    #     login(request, user)
-        
-    #     return Response({"msg": "로그인 성공!!"}, status=status.HTTP_200_OK)
-    
-    # DONE 로그아웃
-    def delete(self, request):
-        logout(request)
-        return Response({"msg": "로그아웃 성공!!"}, status=status.HTTP_200_OK)
-    
+      
     
 # 인가된 사용자만 접근할 수 있는 View 생성
 class OnlyAuthenticatedUserView(APIView):
