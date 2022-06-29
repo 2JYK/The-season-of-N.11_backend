029761a406b505870049267e4076d560e0c19b99
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-29 23:19:27 +0900

    Remove article 앱

--- article/admin.py
+++ article/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from article.models import Article as ArticleModel
-from article.models import Comment as CommentModel
-from article.models import Like as LikeModel
-from article.models import BookMark as BookMarkModel 
-from django.contrib import admin
 
-
-# Bookmark클래스와 BookmarkAdmin클래스를 등록
-admin.site.register(BookMarkModel)
-admin.site.register(ArticleModel)
-admin.site.register(CommentModel)
-admin.site.register(LikeModel)
+# Register your models here.
