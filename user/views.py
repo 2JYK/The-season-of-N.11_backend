fed101df1016ce40d993f3844bbf7117e2077b39
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-29 19:28:30 +0900

    Feat : jwt 관련 내용 추가
    1. SeasonTokenObtainPairSerializer를 임포트 받아와 추가
    2. JWTAuthentication을 사용해 JWT 인증방식 설정
    3. 기존 django session을 사용하던 login 부분 주석 처리

--- user/views.py
+++ user/views.py
@@ -8,6 +8,17 @@ from django.contrib.auth import authenticate, login, logout
 from user.models import User as UserModel
 from user.serializers import UserSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
+from user.jwt_claim_serializer import SeasonTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+# TokenObtainPairView : urls.py에서 import했고, 토큰을 발급받기 위해 사용
+class SeasonTokenObtainPairView(TokenObtainPairView):
+    # serializer_class에 커스터마이징된 시리얼라이저를 넣어 준다.
+    serializer_class = SeasonTokenObtainPairSerializer
+
 class UserView(APIView):
     
     # DONE 회원 정보 조회
@@ -45,20 +56,36 @@ class UserView(APIView):
 class UserAPIView(APIView):
     
     # DONE 로그인
-    def post(self, request):
-        username = request.data.get('username', '')
-        password = request.data.get('password', '')
+    # def post(self, request):
+    #     username = request.data.get('username', '')
+    #     password = request.data.get('password', '')
         
-        user = authenticate(request, username=username, password=password)
+    #     user = authenticate(request, username=username, password=password)
         
-        if not user:
-            return Response({"msg": "존재하지 않는 계정이거나 비밀번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+    #     if not user:
+    #         return Response({"msg": "존재하지 않는 계정이거나 비밀번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
         
-        login(request, user)
+    #     login(request, user)
         
-        return Response({"msg": "로그인 성공!!"}, status=status.HTTP_200_OK)
+    #     return Response({"msg": "로그인 성공!!"}, status=status.HTTP_200_OK)
     
     # DONE 로그아웃
     def delete(self, request):
         logout(request)
         return Response({"msg": "로그아웃 성공!!"}, status=status.HTTP_200_OK)
+    
+    
+# 인가된 사용자만 접근할 수 있는 View 생성
+class OnlyAuthenticatedUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+		
+    # JWT 인증방식 클래스 지정하기
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        # Token에서 인증된 user만 가져온다.
+        user = request.user
+        print(f"user 정보 : {user}")
+        if not user:
+            return Response({"error": "접근 권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({"message": "Accepted"})
