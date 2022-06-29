45ec19cac4232cee14422612c60908e57547e6b5
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-29 19:25:09 +0900

    Update : user 추가

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User as UserModel
+from user.models import User as UserModel
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
