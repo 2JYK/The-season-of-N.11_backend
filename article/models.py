4db083855fa1c833fd258c7bc88202e8105c8cde
GyeongSuKim <rla04722@naver.com>
2022-06-29 20:03:40 +0900

    Feat : rerutn에 id값 추가

--- article/models.py
+++ article/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
     created_at = models.DateTimeField("등록 일자", auto_now_add=True)
     modlfied_at = models.DateTimeField("수정 일자", auto_now=True)
     def __str__(self):
-        return f'{self.user.username} 님이 작성한 Article'
+        return f'id [ {self.id} ] {self.user.username} 님이 작성한 Article'
 
 
 class Comment(models.Model):
@@ -19,18 +19,18 @@ class Comment(models.Model):
     content = models.CharField("내용", max_length=100)
     modlfied_at = models.DateTimeField("수정 일자", auto_now=True)
     def __str__(self):
-        return f'{self.article.title} : {self.content} / {self.user.username}님이 작성한 댓글'
+        return f'id [ {self.id} ] {self.article.title} : {self.content} / {self.user.username}님이 작성한 댓글'
 
     
 class Like(models.Model):
     article = models.ForeignKey('Article', verbose_name="게시글", on_delete=models.CASCADE)
     user = models.ForeignKey('user.User', verbose_name="작성자", on_delete=models.CASCADE)
     def __str__(self):
-        return f'{self.user.username}가 {self.article.title}글을 좋아합니다.'
+        return f'id [ {self.id} ] {self.user.username}가 {self.article.title}글을 좋아합니다.'
 
 
 class BookMark(models.Model):
     article = models.ForeignKey('Article', verbose_name="게시글", on_delete=models.CASCADE)
     user = models.ForeignKey('user.User', verbose_name="작성자", on_delete=models.CASCADE)
     def __str__(self):
-        return f'{self.user.username} 유저가 {self.article.title}글을 북마크 했습니다.'
+        return f'id [ {self.id} ] {self.user.username} 유저가 {self.article.title}글을 북마크 했습니다.'
