863e51696cc1f9603b97d06e587860f0d1c34c01
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-29 11:56:11 +0900

    Feat : custom update 생성

--- user/serializers.py
+++ user/serializers.py
@@ -19,3 +19,13 @@ class UserSerializer(serializers.ModelSerializer):
         
         return user
     
+    def update(self, instance, validated_data):
+        
+        for key,value in validated_data.items():
+            if key == "password":
+                instance.set_password(value)
+                continue
+            setattr(instance, key, value)
+        instance.save()
+        
+        return instance
