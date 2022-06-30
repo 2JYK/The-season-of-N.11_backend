4544cd0feb4bee7fcede4aed115aa8fa93522a35
Daegeun Jeong <wjdeorms27@naver.com>
2022-06-30 11:55:41 +0900

    Feat : jwt 토큰 디코딩
    headers의 authorization에 jwt 토큰값을 넣어 프론트에서 보내고, 이 값을 백에서 받아 디코딩해준다.

--- user/views.py
+++ user/views.py
@@ -14,13 +14,16 @@ from user.jwt_claim_serializer import SeasonTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from the_season.settings import SECRET_KEY, ALGORITHM, SIMPLE_JWT
+import jwt
+
 # TokenObtainPairView : urls.py에서 import했고, 토큰을 발급받기 위해 사용
 class SeasonTokenObtainPairView(TokenObtainPairView):
     # serializer_class에 커스터마이징된 시리얼라이저를 넣어 준다.
     serializer_class = SeasonTokenObtainPairSerializer
 
 class UserView(APIView):
-    
+
     # DONE 회원 정보 조회
     def get(self, request):
         data = UserModel.objects.get(id=request.user.id)
@@ -37,7 +40,12 @@ class UserView(APIView):
 
     # DONE 회원 정보 수정
     def put(self, request):
-        user = UserModel.objects.get(id=request.user.id)
+        # jwt 디코딩
+        client_token = request.headers.get('Authorization', None).replace('Bearer ', '')
+        token = jwt.decode(client_token, SECRET_KEY, algorithms=ALGORITHM) 
+        print(f'token: {token}')
+        
+        user = UserModel.objects.get(id=token["user_id"])
         user_serializer = UserSerializer(user, data=request.data, partial=True)
 
         if user_serializer.is_valid():
@@ -47,24 +55,31 @@ class UserView(APIView):
 
     # DONE 회원 탈퇴
     def delete(self, request):
-        user = UserModel.objects.get(id=request.user.id)
+        # jwt 디코딩
+        client_token = request.headers.get('Authorization', None).replace('Bearer ', '')
+        token = jwt.decode(client_token, SECRET_KEY, algorithms=ALGORITHM) 
+        print(f'token: {token}')
+        
+        user = UserModel.objects.get(id=token["user_id"])
         if user:
             user.delete()
             return Response({"message": "회원탈퇴 성공"}, status=status.HTTP_200_OK)
         return Response({"message": "회원탈퇴 실패"}, status=status.HTTP_400_BAD_REQUEST)
-      
     
-# 인가된 사용자만 접근할 수 있는 View 생성
+# 인가된 사용자의 정보 조회
 class OnlyAuthenticatedUserView(APIView):
+    
     permission_classes = [permissions.IsAuthenticated]
-		
-    # JWT 인증방식 클래스 지정하기
-    authentication_classes = [JWTAuthentication]
+    authentication_classes = [JWTAuthentication] # JWT 인증방식 클래스 지정하기
 
     def get(self, request):
-        # Token에서 인증된 user만 가져온다.
-        user = request.user
-        print(f"user 정보 : {user}")
-        if not user:
+        # Token에서 인증된 사용자의 정보만을 가져온다.
+        client_token = request.headers.get('Authorization', None).replace('Bearer ', '')
+        token = jwt.decode(client_token, SECRET_KEY, algorithms=ALGORITHM)
+        
+        user_info = UserModel.objects.get(id=token["user_id"])
+        serialized_user = UserSerializer(user_info)
+        
+        if not client_token:
             return Response({"error": "접근 권한이 없습니다."}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response({"message": "Accepted"})
+        return Response({"user_info": serialized_user.data}, status=status.HTTP_200_OK)
