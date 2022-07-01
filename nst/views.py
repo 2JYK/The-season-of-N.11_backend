fb0a355e685264e1c0be0bde44cef2355fdead6d
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-01 16:02:49 +0900

    Feat : 머신러닝 class 함수화
    다른 곳에서 import해 사용할 수 있도록 함수로 변형

--- nst/views.py
+++ nst/views.py
@@ -6,33 +6,34 @@ from rest_framework import status
 
 import cv2 
 import numpy as np
+    
+def magic(filestr, style):
+    npimg = np.fromstring(filestr, np.uint8)
+    input_img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    
+    style = cv2.dnn.readNetFromTorch(f'nst/models/{style}')
+    
+    h, w, c = input_img.shape
+    input_img = cv2.resize(input_img, dsize=(500, int(h / w * 500)))
+    MEAN_VALUE = [103.939, 116.779, 123.680]
+    blob = cv2.dnn.blobFromImage(input_img, mean=MEAN_VALUE)
+    style.setInput(blob)
+    output = style.forward()
+    output = output.squeeze().transpose((1, 2, 0)) 
+    output += MEAN_VALUE 
+    output = np.clip(output, 0, 255) 
+    output = output.astype('uint8')
+    
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%s')
+    return cv2.imwrite(f'nst/output/{time}.jpeg', output) 
 
 
 class NstView(APIView):
-
     def post(self, request): 
         user = request.user
-        filestr = request.FILES['input'].read()
-        # 변환 : string data to numpy array
-        npimg = np.fromstring(filestr, np.uint8)
-        # 변환 : numpy array to image
-        input_img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-        
-        style = request.data.get('style', '') 
-        style = cv2.dnn.readNetFromTorch(f'nst/models/{style}')
-        
-        h, w, c = input_img.shape
-        input_img = cv2.resize(input_img, dsize=(500, int(h / w * 500)))
-        MEAN_VALUE = [103.939, 116.779, 123.680]
-        blob = cv2.dnn.blobFromImage(input_img, mean=MEAN_VALUE)
-        style.setInput(blob)
-        output = style.forward()
-        output = output.squeeze().transpose((1, 2, 0)) 
-        output += MEAN_VALUE 
-        output = np.clip(output, 0, 255) 
-        output = output.astype('uint8')
-        
-        time = datetime.now().strftime('%Y-%m-%d %H:%M')
-        output_img = cv2.imwrite(f'nst/output/{time}.jpeg', output)  
+        output_img = magic(
+                filestr=request.FILES['input'].read(),
+                style=request.data.get('style', '') 
+            )
 
         return Response({"output": output_img}, status=status.HTTP_200_OK)
