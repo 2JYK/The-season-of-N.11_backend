9d853cf1022fc856625e971a689b583056bfb7cf
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-01 23:36:06 +0900

    Update : access 토큰 유효시간 연장
    테스트 시 엑세스 만료 시간이 너무 짧아 연장

--- the_season/settings.py
+++ the_season/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
 		# Access 토큰 유효 시간 설정하기
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
 		# Refresh 토큰 유효 시간 설정하기
     'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
 
