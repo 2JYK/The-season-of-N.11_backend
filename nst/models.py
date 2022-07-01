09451ce4e64097a9409107747a6435ab16fa052a
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-01 20:47:11 +0900

    Update : 불필요 부분 주석 처리 및 변수명 변경

--- nst/models.py
+++ nst/models.py
@@ -9,8 +9,8 @@ class Style(models.Model):
 class Image(models.Model):
     style = models.ForeignKey('Style', verbose_name="스타일", on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey('user.User', verbose_name="사용자", on_delete=models.CASCADE)
-    article = models.ForeignKey('article.Article', verbose_name="게시글", on_delete=models.CASCADE)
+    # article = models.ForeignKey('article.Article', verbose_name="게시글", on_delete=models.CASCADE)
     output_img = models.ImageField("결과사진", upload_to="nst/output", height_field=None, width_field=None, max_length=200)
     
     def __str__(self):
-        return f'{self.user} --> {self.output}'
+        return f'{self.user} --> {self.output_img}'
