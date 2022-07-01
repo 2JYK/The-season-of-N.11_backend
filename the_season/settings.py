7b5e3dba1456a82c05459e97761c89a4890e7881
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-01 22:36:47 +0900

    Remove : 사용하지 않는 코드 삭제
    디코딩을 하지 않게 됨으로 인해 알고리즘 코드 삭제

--- the_season/settings.py
+++ the_season/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITHM =  os.environ.get("ALGORITHM")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
