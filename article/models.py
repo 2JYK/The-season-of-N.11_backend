ec732343a80b5bbd0d79c6fb75a7822b12236aee
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-03 15:36:45 +0900

    Feat : image 필드 변경

--- article/models.py
+++ article/models.py
@@ -8,14 +8,15 @@ class Style(models.Model):
 
 class Image(models.Model):
     style = models.ForeignKey('Style', verbose_name="스타일", on_delete=models.SET_NULL, null=True)
-    output_img = models.ImageField("결과사진", upload_to="article/output", height_field=None, width_field=None, max_length=200)
+    output_img = models.FileField("결과사진", upload_to="output/", null=True)
     
     def __str__(self):
         return f'{self.style} -> {self.output_img}'
 
 class Article(models.Model):
     user = models.ForeignKey('user.User', verbose_name="작성자", on_delete=models.CASCADE)
-    image = models.ForeignKey('Image', verbose_name="이미지", on_delete=models.SET_NULL, null=True)
+    # image = models.ForeignKey('Image', verbose_name="이미지", on_delete=models.SET_NULL, null=True)
+    image = models.CharField(max_length=100)
     title = models.CharField("제목", max_length=30)
     content = models.CharField("내용", max_length=100)
     created_at = models.DateTimeField("등록 일자", auto_now_add=True)
